34ac7012a3954b67568cc819f8a07df882f12f18
LukeFiddy <91353389+LukeFiddy@users.noreply.github.com>
2024-12-11 15:54:04 +0000

    adding find_voltages script (#9)
    * adding testing notebook
    
    * add test files
    
    * update test ipynb
    
    * Add a file containing the function to calculate the voltage corrections
    
    * updating script
    
    * changing script file
    
    * add tests for find_votlages script
    
    * remove temp test files
    
    * add dependencies to pyproject.toml
    
    ---------
    
    Co-authored-by: Luke Fiddy <vbz18877@pc0117.cs.diamond.ac.uk>
    Co-authored-by: root <root@pc0117.cs.diamond.ac.uk>
    Co-authored-by: Luke Fiddy <luke.fiddy@diamond.ac.uk>

---
+++ tests/script_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+
+from bimorph_mirror_analysis.maths import find_voltages
+
+# from bimorph_mirror_analysis.maths import find_voltages
+
+
+@pytest.mark.parametrize(
+    ["input_path", "output_path"],
+    [
+        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
+        ["tests/data/16_actuator_data.txt", "tests/data/16_actuator_output.txt"],
+    ],
+)
+def test_find_voltages_correct_output(input_path, output_path):
+    data = np.loadtxt(input_path, delimiter=",")
+    v = -100
+    expected_output = np.loadtxt(output_path, delimiter=",")
+    np.testing.assert_almost_equal(
+        find_voltages(data, v, baseline_voltage_scan=-1), expected_output
+    )
+
+
+def test_find_voltages_index_error_throw():
+    data = np.loadtxt("tests/data/8_actuator_data.txt", delimiter=",")
+    with pytest.raises(IndexError):
+        find_voltages(data, -100, baseline_voltage_scan=12)
+
+
+def test_find_voltages_type_error_throw():
+    data = np.loadtxt("tests/data/8_actuator_data.txt", delimiter=",")
+    with pytest.raises(TypeError):
+        find_voltages(data, -100, baseline_voltage_scan="12")  # type: ignore
