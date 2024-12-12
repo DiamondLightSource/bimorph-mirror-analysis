d4df5f080af92d985d585233230e371d6b6ae562
LukeFiddy <91353389+LukeFiddy@users.noreply.github.com>
2024-12-12 16:03:49 +0000

    Enable pyright strict (#11)
    * Enable Pyright strict mode
    
    * update typing for pyright
    
    ---------
    
    Co-authored-by: Daniel Fernandes <danieljosephfernandes@gmail.com>
    Co-authored-by: Luke Fiddy <luke.fiddy@diamond.ac.uk>

--- src/bimorph_mirror_analysis/maths.py
+++ src/bimorph_mirror_analysis/maths.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 def find_voltages(
-    data: np.ndarray, v: float, baseline_voltage_scan: int = 0
-) -> np.ndarray:
+    data: np.typing.NDArray[np.float64],
+    v: float,
+    baseline_voltage_scan: int = 0,
+) -> np.typing.NDArray[np.float64]:
     """Calculate voltage corrections to apply to bimorph.
 
     Given a matrix of beamline centroid data, with columns of beamline scans at
@@ -23,8 +25,6 @@ def find_voltages(
         beam scan to the target position.
     """
 
-    if not isinstance(baseline_voltage_scan, int):
-        raise TypeError("baseline_voltage_scan must be an integer")
     if baseline_voltage_scan < -data.shape[1] or baseline_voltage_scan >= data.shape[1]:
         raise IndexError(
             f"baseline_voltage_scan is out of range, it must be between\
@@ -35,9 +35,13 @@ def find_voltages(
         data, axis=1
     )  # calculate the response of each actuator by subtracting previous pencil beam
 
-    H = responses / v  # response per unit charge
-    H = np.hstack((np.ones((H.shape[0], 1)), H))  # add columns of 1's to the left of H
-    H_inv = np.linalg.pinv(H)  # calculate the Moore-Penrose pseudo inverse of H
+    interation_matrix = responses / v  # response per unit charge
+    # add columns of 1's to the left of H
+    interation_matrix = np.hstack(
+        (np.ones((interation_matrix.shape[0], 1)), interation_matrix)
+    )
+    # calculate the Moore-Penrose pseudo inverse of H
+    interation_matrix_inv = np.linalg.pinv(interation_matrix)
 
     baseline_voltage_beamline_positions = data[:, baseline_voltage_scan]
 
@@ -45,7 +49,7 @@ def find_voltages(
     Y = target - baseline_voltage_beamline_positions
 
     voltage_corrections = np.matmul(
-        H_inv, Y
+        interation_matrix_inv, Y
     )  # calculate the voltage required to move the centroid to the target position
 
     return voltage_corrections[1:]  # return the voltages
