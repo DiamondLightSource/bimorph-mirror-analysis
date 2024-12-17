a14b85ebe5115cd45ce04259fc1c050a1e55adf4
Luke Fiddy <luke.fiddy@diamond.ac.uk>
2024-12-17 09:13:46 +0000

    change main to read raw data input and output optimal voltages

--- src/bimorph_mirror_analysis/__main__.py
+++ src/bimorph_mirror_analysis/__main__.py
@@ -3,6 +3,11 @@
 from argparse import ArgumentParser
 from collections.abc import Sequence
 
+import numpy as np
+
+from bimorph_mirror_analysis.maths import find_voltages
+from bimorph_mirror_analysis.read_file import read_bluesky_plan_output
+
 from . import __version__
 
 __all__ = ["main"]
@@ -17,7 +22,26 @@ def main(args: Sequence[str] | None = None) -> None:
         action="version",
         version=__version__,
     )
-    parser.parse_args(args)
+    parser.add_argument(
+        "file_path",
+        type=str,
+        help="Path to the file containing the output of the Bluesky plan.",
+    )
+    a = parser.parse_args(args)
+    file_path = a.file_path
+    optimal_voltages = calculate_optimal_voltages(file_path)
+    print(f"The optimal voltages are: {optimal_voltages}")
+
+
+# implement this into main
+def calculate_optimal_voltages(file_path: str) -> np.typing.NDArray[np.float64]:
+    pivoted, initial_voltages, increment = read_bluesky_plan_output(file_path)
+    # numpy array of pencil beam scans
+    data = pivoted[pivoted.columns[1:]].to_numpy()  # type: ignore
+
+    voltage_adjustments = find_voltages(data, increment)  # type: ignore
+    optimal_voltages = initial_voltages + voltage_adjustments
+    return optimal_voltages  # type: ignore
 
 
 if __name__ == "__main__":
