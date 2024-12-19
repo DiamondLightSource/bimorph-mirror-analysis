1bc1dd5f5645064326cc2065aa4dd576dffe3c43
Luke Fiddy <luke.fiddy@diamond.ac.uk>
2024-12-19 10:39:45 +0000

    add typer cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,20 @@
 import subprocess
 import sys
+from unittest.mock import patch
+
+from typer.testing import CliRunner
 
 from bimorph_mirror_analysis import __version__
+from bimorph_mirror_analysis.__main__ import app
+
+runner = CliRunner()
+
+
+def test_app():
+    with patch("bimorph_mirror_analysis.__main__.np.savetxt") as mock_np_save:
+        runner.invoke(app, ["calculate-voltages", "tests/data/raw_data.csv"])
+        mock_np_save.assert_called_once()
+        runner.invoke(app, ["calculate-voltages", "tests/data/raw_data.csv"])
 
 
 def test_cli_version():
