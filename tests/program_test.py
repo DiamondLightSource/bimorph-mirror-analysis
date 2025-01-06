9d975252458ceb199835781612542ffc7edf2b87
LukeFiddy <91353389+LukeFiddy@users.noreply.github.com>
2025-01-06 10:34:06 +0000

    Refactor cli to typer (#15)
    * initial changes to refactor to typer
    
    * fix cli test for typer
    
    * add mocking for calculate_optimal_voltages function
    
    * added unitest to pyproject.toml
    
    * added typer to pyproject.toml
    
    * remove pytest from pyproject.toml
    
    * add typer cli test
    
    * add mocking and fixtures to tests
    
    * add type ignore flag to pandas object
    
    * fix indentation
    
    * add stdout assertion to test_app
    
    * combine the two tests
    add assertion for passed arguments
    rename mock object
    
    * add assertion for increment arg
    
    * change entrypoint from main to app
    
    * add parametrize to test cli with and without output_path arg
    
    * remove print statement
    
    * fix bug, change underscore to hyphen
    
    * fix unbound error on runner result
    
    ---------
    
    Co-authored-by: Luke Fiddy <luke.fiddy@diamond.ac.uk>
    Co-authored-by: root <root@pc0117.cs.diamond.ac.uk>

--- tests/program_test.py
+++ tests/program_test.py
@@ -1,16 +1,35 @@
+from unittest.mock import patch
+
 import numpy as np
-import pytest
+import pandas as pd
 
 from bimorph_mirror_analysis.__main__ import calculate_optimal_voltages
+from bimorph_mirror_analysis.maths import find_voltages
 
 
-@pytest.mark.parametrize(
-    ["input_file", "expected_voltages"],
-    [["tests/data/raw_data.csv", np.array([72.14, 50.98, 18.59])]],
-)
-def test_calculate_optimal_voltages(
-    input_file: str, expected_voltages: np.typing.NDArray[np.float64]
-):
-    voltages = calculate_optimal_voltages(input_file)
-    voltages = np.round(voltages, 2)
-    np.testing.assert_almost_equal(voltages, expected_voltages)
+def test_calculate_optimal_voltages_mocked(raw_data_pivoted: pd.DataFrame):
+    with (
+        patch(
+            "bimorph_mirror_analysis.__main__.read_bluesky_plan_output"
+        ) as mock_read_bluesky_plan_output,
+        patch("bimorph_mirror_analysis.__main__.find_voltages") as mock_find_voltages,
+    ):
+        # set the mock return values
+        mock_read_bluesky_plan_output.return_value = (
+            raw_data_pivoted,
+            np.array([0.0, 0.0, 0.0]),
+            100,
+        )
+        mock_find_voltages.side_effect = find_voltages
+        voltages = calculate_optimal_voltages("input_file")
+        voltages = np.round(voltages, 2)
+        # assert correct voltages calculated
+        np.testing.assert_almost_equal(voltages, np.array([72.14, 50.98, 18.59]))
+
+        # assert mock was called
+        mock_read_bluesky_plan_output.assert_called()
+        mock_read_bluesky_plan_output.assert_called_with("input_file")
+        mock_find_voltages.assert_called()
+        expected_data = raw_data_pivoted[raw_data_pivoted.columns[1:]].to_numpy()  # type: ignore
+        np.testing.assert_array_equal(mock_find_voltages.call_args[0][0], expected_data)  # type: ignore
+        np.testing.assert_almost_equal(mock_find_voltages.call_args[0][1], 100)
