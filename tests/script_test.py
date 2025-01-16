be087daafc1a0ccdc2b364e683b5b5d54145c76b
Luke Fiddy <luke.fiddy@diamond.ac.uk>
2025-01-16 11:51:24 +0000

    add tests for restained voltage calculation function

--- tests/script_test.py
+++ tests/script_test.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
 
-from bimorph_mirror_analysis.maths import find_voltage_corrections
+from bimorph_mirror_analysis.maths import (
+    find_voltage_corrections,
+    find_voltage_corrections_with_restraints,
+)
 
 
 @pytest.mark.parametrize(
@@ -22,7 +25,35 @@ def test_find_voltages_correct_output(
     )
 
 
-def test_find_voltage_corrections_index_error_throw():
-    data = np.loadtxt("tests/data/8_actuator_data.txt", delimiter=",")
+@pytest.mark.parametrize(
+    "actuator_data",
+    [
+        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
+    ],
+    indirect=True,
+)
+def test_find_voltage_corrections_index_error_throw(
+    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+):
+    data, _ = actuator_data
     with pytest.raises(IndexError):
         find_voltage_corrections(data, -100, baseline_voltage_scan=12)
+
+
+@pytest.mark.parametrize(
+    "actuator_data",
+    [
+        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
+        ["tests/data/16_actuator_data.txt", "tests/data/16_actuator_output.txt"],
+    ],
+    indirect=True,
+)
+def test_find_voltage_corrections_with_restraints_correct_output(
+    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+):
+    data, expected_output = actuator_data
+    v = -100
+    np.testing.assert_almost_equal(
+        find_voltage_corrections_with_restraints(data, v, baseline_voltage_scan=-1),
+        expected_output,
+    )
