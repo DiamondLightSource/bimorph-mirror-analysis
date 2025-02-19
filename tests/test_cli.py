4d7a51ce5d485396a6b5e8e3c1695c8010db452f
Luke Fiddy <luke.fiddy@diamond.ac.uk>
2025-02-19 16:40:05 +0000

    fix cli test for plots to use fixed voltage calculation with restraints
    the initial voltages were not mocked properly, was just returning an int because this previously did not throw an error

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -135,7 +135,7 @@ def test_generate_plots(raw_data_pivoted: pd.DataFrame, output_dir: str):
             "bimorph_mirror_analysis.__main__.read_bluesky_plan_output"
         ) as mock_read_bluesky_plan_output,
     ):
-        mock_read_bluesky_plan_output.return_value = [raw_data_pivoted, 0, 100]
+        mock_read_bluesky_plan_output.return_value = [raw_data_pivoted, [0, 0, 0], 100]
         _ = runner.invoke(
             app,
             [
