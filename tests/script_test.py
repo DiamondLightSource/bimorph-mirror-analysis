fc13f924561cab5f5f19e7730df6647f27d6eb30
Luke Fiddy <luke.fiddy@diamond.ac.uk>
2025-02-19 10:09:40 +0000

    change script tests to accomodate addition of initial voltages to fixture

--- tests/script_test.py
+++ tests/script_test.py
@@ -11,19 +11,31 @@ from bimorph_mirror_analysis.maths import (
 @pytest.mark.parametrize(
     "actuator_data",
     [
-        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
-        ["tests/data/16_actuator_data.txt", "tests/data/16_actuator_output.txt"],
+        [
+            "tests/data/8_actuator_data.txt",
+            "tests/data/8_actuator_output.txt",
+            "tests/data/8_actuator_initial_voltages.txt",
+        ],
+        [
+            "tests/data/16_actuator_data.txt",
+            "tests/data/16_actuator_output.txt",
+            "tests/data/16_actuator_initial_voltages.txt",
+        ],
     ],
     indirect=True,
 )
 def test_find_voltages_correct_output(
-    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+    actuator_data: tuple[
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+    ],
 ):
-    data, expected_output = actuator_data
+    data, expected_corrections, _ = actuator_data
     v = -100
     np.testing.assert_almost_equal(
         find_voltage_corrections(data, v, baseline_voltage_scan=-1),
-        expected_output,
+        expected_corrections,
         decimal=2,
     )
 
@@ -31,14 +43,22 @@ def test_find_voltages_correct_output(
 @pytest.mark.parametrize(
     "actuator_data",
     [
-        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
+        [
+            "tests/data/8_actuator_data.txt",
+            "tests/data/8_actuator_output.txt",
+            "tests/data/8_actuator_initial_voltages.txt",
+        ],
     ],
     indirect=True,
 )
 def test_find_voltage_corrections_index_error_throw(
-    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+    actuator_data: tuple[
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+    ],
 ):
-    data, _ = actuator_data
+    data, *_ = actuator_data
     with pytest.raises(IndexError):
         find_voltage_corrections(data, -100, baseline_voltage_scan=12)
 
@@ -46,21 +66,33 @@ def test_find_voltage_corrections_index_error_throw(
 @pytest.mark.parametrize(
     "actuator_data",
     [
-        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
-        ["tests/data/16_actuator_data.txt", "tests/data/16_actuator_output.txt"],
+        [
+            "tests/data/8_actuator_data.txt",
+            "tests/data/8_actuator_output.txt",
+            "tests/data/8_actuator_initial_voltages.txt",
+        ],
+        [
+            "tests/data/16_actuator_data.txt",
+            "tests/data/16_actuator_output.txt",
+            "tests/data/16_actuator_initial_voltages.txt",
+        ],
     ],
     indirect=True,
 )
 def test_find_voltage_corrections_with_restraints_correct_output(
-    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+    actuator_data: tuple[
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+    ],
 ):
-    data, expected_output = actuator_data
+    data, expected_corrections, _ = actuator_data
     v = -100
     np.testing.assert_almost_equal(
         find_voltage_corrections_with_restraints(
             data, v, (-1000, 1000), 500, baseline_voltage_scan=-1
         ),
-        expected_output,
+        expected_corrections,
         decimal=1,
     )
 
@@ -68,14 +100,22 @@ def test_find_voltage_corrections_with_restraints_correct_output(
 @pytest.mark.parametrize(
     "actuator_data",
     [
-        ["tests/data/8_actuator_data.txt", "tests/data/8_actuator_output.txt"],
+        [
+            "tests/data/8_actuator_data.txt",
+            "tests/data/8_actuator_output.txt",
+            "tests/data/8_actuator_initial_voltages.txt",
+        ],
     ],
     indirect=True,
 )
 def test_find_voltage_corrections_with_restraints_index_error_throw(
-    actuator_data: tuple[np.typing.NDArray[np.float64], np.typing.NDArray[np.float64]],
+    actuator_data: tuple[
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+        np.typing.NDArray[np.float64],
+    ],
 ):
-    data, _ = actuator_data
+    data, *_ = actuator_data
     with pytest.raises(IndexError):
         find_voltage_corrections_with_restraints(
             data, -100, (-1000, 1000), 500, baseline_voltage_scan=12
